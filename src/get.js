5adec7517220f9db64f0f249b6c963a94b8624b9
spencersa <spencersa715@gmail.com>
2021-02-07 13:09:06 -0600

    Initial commit

---
+++ src/get.js
@@ -0,0 +1,36 @@
+'use strict';
+let AWS = require("aws-sdk");
+
+module.exports.handle = async (event) => {
+  try {
+    let dynamoDb = new AWS.DynamoDB.DocumentClient({
+      service: new AWS.DynamoDB(
+        {
+          apiVersion: "2012-08-10"
+        }),
+      convertEmptyValues: true
+    });
+
+    var params = {
+      TableName: "rollable-tables",
+      IndexName: 'userId-index',
+      KeyConditionExpression: 'userId = :userId',
+      ExpressionAttributeValues: { ':userId': event.userId }
+    };
+
+    var result = await dynamoDb.query(params).promise();
+
+    var items = result.Items;
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(items)
+    };
+
+  } catch (error) {
+    console.log(error);
+    return {
+      statusCode: 500
+    };
+  }
+};
---
+++ src/post.js
@@ -0,0 +1,30 @@
+'use strict';
+let AWS = require("aws-sdk");
+
+module.exports.handle = async (event) => {
+  try {
+    var dynamoDb = new AWS.DynamoDB();
+
+    var params = {
+      TableName: 'rollable-tables',
+      Item: {
+        'userId': { S: event.userId },
+        'tableId': { S: event.tableId },
+        'tableName': { S: event.tableName },
+        'tags': { SS: event.tags },
+        'data': { S: JSON.stringify(event.data) },
+      }
+    };
+
+    var result = await dynamoDb.putItem(params).promise();
+    return {
+      statusCode: 200
+    };
+
+  } catch (error) {
+    console.log(error);
+    return {
+      statusCode: 500
+    };
+  }
+};
